d9098e6adf8f6602d62773ca312680dd6cda4d84
nyattonguyen <nyattonguyen@gmail.com>
2023-06-24 11:50:59 +0700

    update v.v5

--- note_app/client/src/util/contants.js
+++ note_app/client/src/util/contants.js
@@ -1,3 +1,3 @@
 export const GRAPHQL_SERVER = "https://note-app-japan.onrender.com";
 export const GRAPHQL_SUBSCRIPTION_ENDPOINT =
-  "ws://https://note-app-japan.onrender.com/graphql";
+  "wss://https://note-app-japan.onrender.com/graphql";
